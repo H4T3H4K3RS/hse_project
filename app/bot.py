3f208a8b48a69fdca17e61e4feb976fe2f11c47b
Ovchinnikov Vsevolod <sevavov.2004@gmail.com>
2020-06-06 14:58:04 +0300

    New links bot command. Added heroku support

--- app/bot.py
+++ app/bot.py
@@ -21,13 +21,52 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+start_msg = f'Пришлите в сообщении API\-ключ, полученный по [ссылке]({settings.HOST}{reverse("api_account_key")})'
+help_msg = '1\. Просто пришлите текст, содержащий ссылки, в сообщении\. \(Все ссылки из него будут сохранены\)\.\n2\. ' \
+           'Перешлите сообщение, содержащее ссылки, боту\. \(Все ссылки из него будут сохранены\)\. '
+
+
+def escape(text):
+    symbols = [".", "(", ")", "-"]
+    for symbol in symbols:
+        text = text.replace(symbol, "\\" + symbol)
+    return text
+
 
 def start(update, context):
-    update.message.reply_text(f'Пришлите в сообщении API\-ключ, полученный по [ссылке](https://linkit.herokuapp.com)', parse_mode=telegram.ParseMode.MARKDOWN_V2)
+    update.message.reply_text(start_msg, parse_mode=telegram.ParseMode.MARKDOWN_V2)
+
+
+def links(update, context):
+    chat_id = str(update.message.from_user.id)
+    try:
+        bot_key = BotKey.objects.get(chat_id=chat_id)
+        all_links = Link.objects.filter(folder__user=bot_key.user).order_by("-rating")
+        answer = ""
+        cnt = 1
+        for link in all_links:
+            answer += f"{cnt}\. [{link.rating}] [{escape(link.link)}]({link.link})\n"
+            cnt += 1
+        if answer == "":
+            answer = "Вы ещё не добавили ни одну ссылку\.\n" + help_msg
+        update.message.reply_text(answer, parse_mode=telegram.ParseMode.MARKDOWN_V2)
+    except BotKey.DoesNotExist:
+        update.message.reply_text("Вы не авторизованы\.")
+
+
+def logout(update, context):
+    chat_id = str(update.message.from_user.id)
+    try:
+        bot_key = BotKey.objects.get(chat_id=chat_id)
+        bot_key.chat_id = ""
+        bot_key.save()
+        update.message.reply_text("Вы успешны вышли из аккаунта\!", parse_mode=telegram.ParseMode.MARKDOWN_V2)
+    except BotKey.DoesNotExist:
+        update.message.reply_text("Вы не авторизованы\.", parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
 
 def help(update, context):
-    update.message.reply_text('Help!')
+    update.message.reply_text(help_msg, parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
 
 def add_link(update, context):
@@ -44,55 +83,48 @@ def add_link(update, context):
         for i in extractor.find_urls(update.message.text):
             try:
                 link = Link.objects.get(folder=folder, link=i)
-                answer += f'{cnt}. {link.link} - уже существует в подборке\n'
+                answer += f'{cnt}\. {escape(link.link)} \- уже существует в подборке\n'
             except Link.DoesNotExist:
                 link = Link(folder=folder, link=i)
                 link.save()
-                answer += f'{cnt}. {link.link} - успешно сохранена\n'
+                answer += f'{cnt}\. {escape(link.link)} \- успешно сохранена\n'
             cnt += 1
         if answer == "":
             answer = "В вашем сообщении не обнаружено ни одной ссылки."
-        update.message.reply_text(answer)
+        update.message.reply_text(answer, parse_mode=telegram.ParseMode.MARKDOWN_V2)
     except BotKey.DoesNotExist:
-        try:
-            bot_key = BotKey.objects.get(key=update.message.text)
-            bot_key.chat_id = chat_id
-            bot_key.save()
-            update.message.reply_text(f"Здравствуйте, {bot_key.user.username}")
-        except BotKey.DoesNotExist:
-            update.message.reply_text("Неправильный API-ключ.")
+        bot_keys = BotKey.objects.filter(key=update.message.text)
+        if len(bot_keys) != 0:
+            flag = False
+            for i in range(len(bot_keys)):
+                if bot_keys[i].chat_id == "":
+                    bot_keys[i].chat_id = chat_id
+                    flag = True
+                elif len(bot_keys) == i - 1:
+                    bot_keys[i] = BotKey(key=update.message.text, chat_id=chat_id, user=bot_keys[i].user)
+                    flag = True
+                bot_keys[i].save()
+                if flag:
+                    update.message.reply_text(f"Здравствуйте, {bot_keys[i].user.username}\n" + help_msg,
+                                              parse_mode=telegram.ParseMode.MARKDOWN_V2)
+        else:
+            update.message.reply_text("Неправильный API-ключ.", parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
 
 def error(update, context):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
     updater = Updater(api_key, use_context=True)
-
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-
-    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("links", links))
+    dp.add_handler(CommandHandler('logout', logout))
     dp.add_handler(MessageHandler(Filters.text, add_link))
-
-    # log all errors
     dp.add_error_handler(error)
-
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
