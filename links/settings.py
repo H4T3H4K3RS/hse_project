ea482297a888da5d78f47be962131b650f801038
Ovchinnikov Vsevolod <sevavov.2004@gmail.com>
2020-06-10 13:00:29 +0300

    Fixing ssl

--- links/settings.py
+++ links/settings.py
@@ -158,7 +158,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-SECURE_SSL_REDIRECT = os.getenv("SSL", False)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
