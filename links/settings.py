3b227e73166cd03861c3e4e10744aba92fdc35b7
Ovchinnikov Vsevolod <sevavov.2004@gmail.com>
2020-07-22 20:30:56 +0300

    Fxied disable cache

--- links/settings.py
+++ links/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "disable_cache_headers.middleware.DisableCacheControl",
 ]
 
 ROOT_URLCONF = 'links.urls'
